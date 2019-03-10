960cd0c84471b49c7d56273f943802e32dfee15d
Simon R <nexus-uw@users.noreply.github.com>
2019-03-09 18:40:05 -0800

    rebuilt

--- _site/build.bash
+++ _site/build.bash
@@ -1,2 +1,2 @@
-docker run --rm --label=jekyll --volume=$(pwd):/srv/jekyll -it -p 127.0.0.1:4000:4000 nexusuw/nexus-uw.github.io jekyll build .
+docker run --rm --label=jekyll --volume=$(pwd):/srv/jekyll -it -p 127.0.0.1:4000:4000 jekyll/jekyll jekyll build .
 
--- _site/run-docker-jekyll.bash
+++ _site/run-docker-jekyll.bash
@@ -1,2 +1,2 @@
-docker run --rm --label=jekyll --volume=$(pwd):/srv/jekyll -it -p 4000:4000 nexusuw/nexus-uw.github.io jekyll serve .
+docker run --rm --label=jekyll --volume=$(pwd):/srv/jekyll -it -p 4000:4000 jekyll/jekyll jekyll serve .
 
